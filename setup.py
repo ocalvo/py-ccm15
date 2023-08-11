5a68f4bc04aea3f3991f46161bef9ef5ef76c3c1
Oscar Calvo <oscar@calvonet.com>
2023-08-10 19:29:00 -0600

    Release 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="py-ccm15",
-    version="0.0.2",
+    version="0.0.3",
     author="Oscar Calvo",
     author_email="oscar@calvonet.com",
     description="A package to control Midea CCM15 data converter modules",
