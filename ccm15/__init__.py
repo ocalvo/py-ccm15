0f238202200e7dc5547f49b49c7a3137376fdfbb
Oscar Calvo <oscar@calvonet.com>
2023-08-10 19:02:30 -0600

    All test pass, 2 warnings

--- ccm15/__init__.py
+++ ccm15/__init__.py
@@ -1,6 +1,7 @@
 """ Init file """
 
 from .CCM15Device import CCM15Device
+from .CCM15SlaveDevice import CCM15SlaveDevice, TemperatureUnit
 
-__all__ = ['CCM15Device']
+__all__ = ['CCM15Device', 'CCM15SlaveDevice', 'TemperatureUnit']
 
